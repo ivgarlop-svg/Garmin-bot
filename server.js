3f958de9e5ff0bd5d20b17e26cf837551aa63f7b
ivgarlop-svg <ivgarlop@gmail.com>
2026-03-11 15:15:04 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { GarminConnect } = require('garmin-connect');
+const app = express();
+
+app.get('/datos', async (req, res) => {
+  try {
+    const client = new GarminConnect();
+    await client.login(process.env.GARMIN_EMAIL, process.env.GARMIN_PASSWORD);
+    const sleepData = await client.getSleepData();
+    res.json(sleepData);
+  } catch (error) {
+    res.status(500).json({ error: "Error de conexión" });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
